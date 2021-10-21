0ccffbe7f9578e4a622b91d9a4d01474e87de146
supriyo15998 <supriyo15998@gmail.com>
2021-10-21 15:43:35 +0530

    new program added and warnings fixed #2

--- src/Alternating_Characters.java
+++ src/Alternating_Characters.java
@@ -3,28 +3,21 @@ import java.util.*;
 public class Alternating_Characters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		ArrayList<Character>list=new ArrayList<Character>();
-		for(int i=0;i<n;i++)
-		{
-			String str=sc.next();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
 			check(str);
 		}
+		sc.close();
 	}
-	public static void check(String str)
-	{
-		int count=0;
-		char[] a= str.toCharArray();
-//		for(int i=0;i<a.length;i++)
-//		{
-//			System.out.println(a[i]);
-//		
-//		}
-		for(int i=0;i<a.length-1;i++)
-		{
-			if(a[i]==a[i+1])
+
+	public static void check(String str) {
+		int count = 0;
+		char[] a = str.toCharArray();
+
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] == a[i + 1])
 				count++;
 		}
 		System.out.println(count);
