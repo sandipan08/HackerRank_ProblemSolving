ae8375ae51f6ce527ec5669e2880ee82d2a9d577
supriyo15998 <supriyo15998@gmail.com>
2021-10-21 15:07:33 +0530

    new program added and warnings fixed

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 public class CamelCase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		int count=0;
-		for(int i=0;i<str.length();i++)
-		{
-			char a=str.charAt(i);
-			if(Character.isUpperCase(a)==true)
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		sc.close();
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			char a = str.charAt(i);
+			if (Character.isUpperCase(a) == true)
 				count++;
 		}
-		if(Character.isLowerCase(str.charAt(0))==true)
+		if (Character.isLowerCase(str.charAt(0)) == true)
 			count++;
-		  System.out.print(count);
+		System.out.print(count);
 	}
 
 }
